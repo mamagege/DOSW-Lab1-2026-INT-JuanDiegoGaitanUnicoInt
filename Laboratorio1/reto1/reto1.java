5864e599678fd371fa1e3598a1a349045d12faa0
unknown <juan.gaitan-p@labinfo.is.escuelaing.edu.co>
2026-08-04 16:00:50 -0500

    Implementacion reto 1

---
+++ Laboratorio1/reto1/reto1.java
@@ -0,0 +1,12 @@
+package reto1;
+
+public class reto1 {
+    public static void main(String[] args) {
+        LectorEstudiantes lector = new LectorEstudiantes();
+        lector.leerDatos();
+
+        MensajeBienvenida mensaje = new MensajeBienvenida();
+        mensaje.creadorMensaje(lector.getEstudiantes());
+
+    }
+}
---
+++ out/production/DOSW-Lab1-2026-INT-JuanDiegoGaitanUnicoInt/README.md
@@ -0,0 +1,1 @@
+# DOSW-Lab1-2026-INT-JuanDiegoGaitanUnicoInt
